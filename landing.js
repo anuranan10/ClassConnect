d3d49fa0ed8f6c96d4e65c9338c60d312db26692
anuranan10 <bharada3@my.erau.edu>
2025-06-01 16:46:40 -0400

    qr code generation

--- landing.js
+++ landing.js
@@ -2,7 +2,7 @@ let interval;
 let timeLeft = 30;
 
 async function generateQRCode() {
-  const res = await fetch("/generate-token");
+  const res = await fetch("https://3a254ac9-4201-41e3-838d-f156d6044a28-00-xtkv165zfgnh.worf.replit.dev/generate-token");
   const { qrImage } = await res.json();
   document.getElementById("qr").innerHTML = `<img src="data:image/png;base64,${qrImage}" width="250"/>`;
 }
