8b4928f84c7be3f70860f5f696a1ec10458d6dbb
anuranan10 <bharada3@my.erau.edu>
2025-06-01 15:48:28 -0400

    updating professor's dashboard

---
+++ landing.js
@@ -0,0 +1,22 @@
+let interval;
+let timeLeft = 30;
+
+async function generateQRCode() {
+  const res = await fetch("/generate-token");
+  const { qrImage } = await res.json();
+  document.getElementById("qr").innerHTML = `<img src="data:image/png;base64,${qrImage}" width="250"/>`;
+}
+
+function startQRCodeCycle() {
+  clearInterval(interval);
+  timeLeft = 30;
+  generateQRCode();
+
+  interval = setInterval(() => {
+    timeLeft--;
+    document.getElementById("timer").textContent = `Expires in ${timeLeft}s`;
+
+    if (timeLeft % 7 === 0) generateQRCode();
+    if (timeLeft <= 0) clearInterval(interval);
+  }, 1000);
+}
